611b77249d238f5c15b38ec84bdf6d148f95dee3
FabienCallot <fab.callot@gmail.com>
2022-09-06 11:32:08 +0200

    functions questions 1 & 2

--- src/index.js
+++ src/index.js
@@ -31,5 +31,9 @@ function reverseMove(arr) {
   return reversedString;
 }
 
-const test = reverseMove(paper);
-console.log(test);
+function askQuestion1() {
+  console.log("Rock Paper Scissors? (press 1, 2 or 3)\n");
+}
+function askQuestion2() {
+  console.log("Please press 1, 2 or 3 for play\n");
+}

8e26cbdaa3c95024a3266d531a4d1fe4f3c613f4
FabienCallot <fab.callot@gmail.com>
2022-09-06 11:28:40 +0200

    create reverse function for display a reversed sign

--- src/index.js
+++ src/index.js
@@ -8,3 +8,28 @@ const reader = readline.createInterface({
 });
 
 const clear = () => console.log("\x1B[2J\x1B[0f");
+
+function reverseMove(arr) {
+  let reversedString = [];
+  arr.forEach((element) => {
+    const splited = element.split("");
+    const indexOfParenthesis1 = splited.indexOf(")");
+    const indexOfParenthesis2 = splited.indexOf("(");
+    if (indexOfParenthesis1 !== -1) {
+      splited[indexOfParenthesis1] = "(";
+    }
+    if (indexOfParenthesis2 !== -1) {
+      splited[indexOfParenthesis2] = ")";
+    }
+    const reversed = splited.reverse();
+    const joined = reversed.join("");
+    reversedString.push(joined);
+  });
+  if (arr === scissors) {
+    reversedString[2] = " (________(__)     ";
+  }
+  return reversedString;
+}
+
+const test = reverseMove(paper);
+console.log(test);
